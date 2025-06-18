8bb142ae448d625b72059b300b43c5a37bceee8e
Haoyu_Pan <34794373+haoyupan2003@users.noreply.github.com>
2025-06-17 19:29:35 -0700

    Update barcelona.py

--- algorithmExercise/barcelona.py
+++ algorithmExercise/barcelona.py
@@ -1,14 +1,11 @@
 # https://open.kattis.com/problems/barcelona
 
 n, b = map(int, input().split())
-
 bags = list(map(int, input().split()))
-
 index = bags.index(b)
-
 if index == 0:
     print("fyrst")
 elif index == 1:
     print("naestfyrst")
 else:
-    print(f"{index} fyrst")
+    print(f"{index + 1} fyrst")
