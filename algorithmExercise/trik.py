f86ce2ecfbfec7ad83baf855b1adcf3e1acef447
Haoyu_Pan <34794373+haoyupan2003@users.noreply.github.com>
2025-06-20 23:23:47 -0700

    Create trik.py

---
+++ algorithmExercise/trik.py
@@ -0,0 +1,14 @@
+# https://open.kattis.com/problems/trik
+
+moves = input()
+cups = [1, 0, 0]
+
+for move in moves:
+    if move == 'A':
+        cups[0], cups[1] = cups[1], cups[0]
+    elif move == 'B':
+        cups[1], cups[2] = cups[2], cups[1]
+    elif move == 'C':
+        cups[0], cups[2] = cups[2], cups[0]
+
+print(cups.index(1) + 1)
