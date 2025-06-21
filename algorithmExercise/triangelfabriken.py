dfad9a4e0a7da621cf6c7205ee5c8054887f2b2f
Haoyu_Pan <34794373+haoyupan2003@users.noreply.github.com>
2025-06-20 23:22:45 -0700

    Create triangelfabriken.py

---
+++ algorithmExercise/triangelfabriken.py
@@ -0,0 +1,12 @@
+# https://open.kattis.com/problems/triangelfabriken
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+if a == 90 or b == 90 or c == 90:
+    print("Ratvinklig Triangel")
+elif a > 90 or b > 90 or c > 90:
+    print("Trubbig Triangel")
+else:
+    print("Spetsig Triangel")
