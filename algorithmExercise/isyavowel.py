5e5c2618cece9f413110ff06cac0eb0949b41f66
Haoyu_Pan <34794373+haoyupan2003@users.noreply.github.com>
2025-06-26 18:59:38 -0700

    Update isyavowel.py

--- algorithmExercise/isyavowel.py
+++ algorithmExercise/isyavowel.py
@@ -1,1 +1,8 @@
 # https://open.kattis.com/problems/isyavowel
+
+word = input().strip()
+vowels_without_y = {'a', 'e', 'i', 'o', 'u'}
+vowels_with_y = {'a', 'e', 'i', 'o', 'u', 'y'}
+count_without_y = sum(1 for char in word if char in vowels_without_y)
+count_with_y = sum(1 for char in word if char in vowels_with_y)
+print(count_without_y, count_with_y)
