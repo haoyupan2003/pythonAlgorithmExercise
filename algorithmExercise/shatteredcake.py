5559dbe115a52a097d3abffd245455ee1775fb2c
Haoyu_Pan <34794373+haoyupan2003@users.noreply.github.com>
2025-06-29 22:11:48 -0700

    Update shatteredcake.py

--- algorithmExercise/shatteredcake.py
+++ algorithmExercise/shatteredcake.py
@@ -1,9 +1,11 @@
 # https://open.kattis.com/problems/shatteredcake
 
-W = int(input())
-N = int(input())
+import sys
+
+W = int(sys.stdin.readline())
+N = int(sys.stdin.readline())
 total_area = 0
 for _ in range(N):
-    w, l = map(int, input().split())
+    w, l = map(int, sys.stdin.readline().split())
     total_area += w * l
 print(total_area // W)
