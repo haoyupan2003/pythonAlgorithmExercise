f26434940c9989e38dbe452ab26321328f34a377
Haoyu_Pan <34794373+haoyupan2003@users.noreply.github.com>
2025-07-02 23:53:18 -0700

    Update sibice.py

--- algorithmExercise/sibice.py
+++ algorithmExercise/sibice.py
@@ -1,1 +1,13 @@
 # https://open.kattis.com/problems/sibice
+
+import math
+
+n, w, h = map(int, input().split())
+max_length = math.sqrt(w**2 + h**2)
+
+for _ in range(n):
+    match_length = int(input())
+    if match_length <= max_length:
+        print("DA")
+    else:
+        print("NE")
