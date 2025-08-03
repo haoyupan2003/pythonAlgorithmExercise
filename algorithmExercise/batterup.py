80e1ae2c7fd92b1af20540f9c7eaa8ea36bab017
Haoyu_Pan <34794373+haoyupan2003@users.noreply.github.com>
2025-08-03 00:24:12 -0700

    Update batterup.py

--- algorithmExercise/batterup.py
+++ algorithmExercise/batterup.py
@@ -1,1 +1,15 @@
 # https://open.kattis.com/problems/batterup
+
+N = int(input())
+at_bats = list(map(int, input().split()))
+
+total_bases = 0
+official_at_bats = 0
+
+for result in at_bats:
+    if result != -1:
+        total_bases += result
+        official_at_bats += 1
+
+slugging_percentage = total_bases / official_at_bats
+print(slugging_percentage)
