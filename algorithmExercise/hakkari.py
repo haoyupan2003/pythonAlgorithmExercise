548439904807cf0f0196d5e8880f56c49705dd8f
Haoyu_Pan <34794373+haoyupan2003@users.noreply.github.com>
2025-08-11 23:58:08 -0700

    Update hakkari.py

--- algorithmExercise/hakkari.py
+++ algorithmExercise/hakkari.py
@@ -1,1 +1,14 @@
 # https://open.kattis.com/problems/hakkari
+
+R, C = map(int, input().split())
+mines = []
+
+for r in range(1, R + 1):
+    row = input().strip()
+    for c in range(1, C + 1):
+        if row[c - 1] == '*':
+            mines.append((r, c))
+
+print(len(mines))
+for r, c in mines:
+    print(r, c)
