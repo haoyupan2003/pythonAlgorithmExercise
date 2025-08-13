f5d39d371ea948cdc86f954aa652b6cb9bd9a35d
Haoyu_Pan <34794373+haoyupan2003@users.noreply.github.com>
2025-08-12 21:46:09 -0700

    Update grafaholur.py

--- algorithmExercise/grafaholur.py
+++ algorithmExercise/grafaholur.py
@@ -1,1 +1,10 @@
 # https://open.kattis.com/problems/grafaholur
+
+w1 = int(input())
+h1 = int(input())
+v1 = int(input())
+w2 = int(input())
+v2 = int(input())
+
+t2 = h1 * w1 * v2 / (w2 * v1)
+print(f"{t2:.15f}")
