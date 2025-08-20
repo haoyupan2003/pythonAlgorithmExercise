2a2a5e2d2d776987650686e2daae01f8af887726
Haoyu_Pan <34794373+haoyupan2003@users.noreply.github.com>
2025-08-19 23:36:48 -0700

    Update edays.py

---
+++ algorithmExercise/edays.py
@@ -0,0 +1,10 @@
+# Read inputs
+d = int(input())   # distance to the capitol
+n = int(input())   # number of students (not used)
+t = int(input())   # temperature (not used)
+
+# Calculate total miles (to the capitol and back)
+total_miles = 2 * d
+
+# Print result
+print(total_miles)
