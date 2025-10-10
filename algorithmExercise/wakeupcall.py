8254aa4236156b13a34eccf69cf76dc4ce7b9d9a
Haoyu_Pan <34794373+haoyupan2003@users.noreply.github.com>
2025-10-10 11:20:27 -0700

    Update wakeupcall.py

--- algorithmExercise/wakeupcall.py
+++ algorithmExercise/wakeupcall.py
@@ -1,1 +1,18 @@
 # https://open.kattis.com/problems/wakeupcall
+n, m = map(int, input().split())
+s1c = s2c = 0
+s1 = input().split()
+s2 = input().split()
+
+for i in range(n):
+    s1c += int(s1[i])
+
+for j in range(m):
+    s2c += int(s2[j])
+
+if s1c > s2c:
+    print("Button 1")
+elif s1c < s2c:
+    print("Button 2")
+else:
+    print("Oh no")
