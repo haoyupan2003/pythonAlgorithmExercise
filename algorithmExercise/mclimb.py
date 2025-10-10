904d5c3efe6a2ccedad086c3e20594de61b5a12f
Haoyu_Pan <34794373+haoyupan2003@users.noreply.github.com>
2025-10-09 22:25:02 -0700

    Create mclimb.py

---
+++ algorithmExercise/mclimb.py
@@ -0,0 +1,7 @@
+# https://open.kattis.com/problems/mclimb
+n = int(input())  # number of rocks
+c = int(input())  # cost per liter
+year = int(input())  # year (not needed)
+
+total_cost = n * c
+print(total_cost)
