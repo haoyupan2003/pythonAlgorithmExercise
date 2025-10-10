f48789cc89d088a16b912f50ca4741a25fef6d2c
Haoyu_Pan <34794373+haoyupan2003@users.noreply.github.com>
2025-10-09 22:26:20 -0700

    Update parkingpandemonium.py

---
+++ algorithmExercise/parkingpandemonium.py
@@ -0,0 +1,7 @@
+# https: // open.kattis.com/problems/parkingpandemonium
+t = int(input())  # time per lap
+k = int(input())  # temperature in Kelvin (not needed)
+n = int(input())  # number of laps
+
+total_time = t * n
+print(total_time)
