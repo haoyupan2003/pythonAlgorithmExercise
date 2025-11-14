e13e1a9d4b924242fc46e0d27ee7139da3fde544
Haoyu_Pan <34794373+haoyupan2003@users.noreply.github.com>
2025-11-13 21:11:42 -0800

    Update twosum2.py

--- algorithmExercise/twosum2.py
+++ algorithmExercise/twosum2.py
@@ -1,1 +1,10 @@
 # https://leetcode.com/problems/two-sum/
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        for i, num in enumerate(nums):
+            diff = target - num
+            if diff in seen:
+                return [seen[diff], i]
+            seen[num] = i
